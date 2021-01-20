f850d4c6eb1ec8b61e03a1a324ff750678982dbc
kdm1jkm <kdm1jkmm@gmail.com>
2021-01-20 20:09:59 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+
+namespace DeveloperRPG
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var window = new RenderWindow(new VideoMode(1280, 720), "Hello, World!");
+            window.Closed += (sender, _) => { ((Window) sender)?.Close(); };
+            
+            window.SetFramerateLimit(60);
+
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+                window.Clear(Color.Black);
+                
+                window.Display();
+            }
+        }
+    }
+}
